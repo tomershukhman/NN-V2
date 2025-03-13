a5271a56d7e06dda0ac1bef98e0b8b55fd39407e
Tomer Shukhman <tomer.shukhman@gmail.com>
2025-03-13 02:46:25 +0200

    back  to MAX_DETECTIONS 20

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ TRAIN_NMS_THRESHOLD = 0.45
 # Inference thresholds (more strict)
 CONFIDENCE_THRESHOLD = 0.5  # Increased to reduce false positives
 NMS_THRESHOLD = 0.3  # Reduced to prevent duplicate detections
-MAX_DETECTIONS = 4  # Reduced maximum detections per image
+MAX_DETECTIONS = 20  # Reduced maximum detections per image
 
 # Visualization parameters
 TENSORBOARD_TRAIN_IMAGES = 20  # Number of training images to show in tensorboard
